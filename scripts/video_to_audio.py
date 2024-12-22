2374d9551143ed258100976eeb5be143b65148ed
Sunny <sunny.22scse1012788@galgotiasuniversity.edu.in>
2024-12-22 16:03:46 +0530

    Final code changes

--- scripts/video_to_audio.py
+++
@@ -1,7 +0,0 @@
-from moviepy import VideoFileClip
-
-# Load the video file (using raw string or double backslashes)
-video = VideoFileClip(r"#")
-
-# Extract audio and write it to an MP3 file (using raw string or double backslashes)
-video.audio.write_audiofile(r"#")
--- scripts/whisper_transcription.py
+++
@@ -1,6 +0,0 @@
-#pip install transformers torch
-import torch
-from transformers import pipeline
-whisper = pipeline("automatic-speech-recognition", "openai/whisper-larrge-v3",torch_dtype=torch.float16, device="cuda:0")
-transcription = whisper(r"#")
-print(transcription["text"])
